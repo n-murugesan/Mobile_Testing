9d51efa9b2be4468d236cd439e6d174fd49c2255
n-murugesan <nithin.murugesan@alvaria.com>
2025-08-20 17:37:13 +0530

    Update run-mob.sh

--- run-mob.sh
+++ run-mob.sh
@@ -1,64 +1,32 @@
 #!/bin/bash
 
-# Load environment variables from .env file
-source .env
+EMULATOR_PATH=$ANDROID_HOME/emulator/emulator
+ADB_PATH=$ANDROID_HOME/platform-tools/adb
 
-# Load configuration settings based on environment
-if [ "$ENVIRONMENT" = "local" ]; then
-  # Load local configuration settings
-  echo "Running tests locally"
-  # Set local-specific variables
-  avdName="Pixel_9_Pro"
-  androidHome="$ANDROID_HOME"
+echo "🚀 Getting AVD list..."
+avdList=$($EMULATOR_PATH -list-avds)
 
-elif [ "$ENVIRONMENT" = "cloud" ]; then
-  # Load cloud configuration settings
-  echo "Running tests in cloud"
-  # Set cloud-specific variables
-  aavdName="Pixel_9_Pro"
-  androidHome="$ANDROID_HOME"
-else
-  echo "Invalid environment"
-  exit 1
-fi
+echo "AVD list: $avdList"
 
-# Starting Android Emulator
-echo "Starting Android Emulator: $avdName"
-"$androidHome/emulator/emulator" -avd "$avdName" -no-snapshot-save &
+avdName=$(echo "$avdList" | head -n 1)
 
-# Waiting for emulator to boot...
-echo "Waiting for emulator to boot..."
+echo "Selected AVD: $avdName"
 
-# Loop until emulator is ready
+$EMULATOR_PATH -avd "$avdName" -no-snapshot-save &
+sleep 10
+
+echo "⌛ Waiting for emulator to boot..."
 while true; do
-#   deviceState=$(adb get-state 2>/dev/null)
-#   bootComplete=$(adb shell getprop sys.boot_completed 2>/dev/null | tr -d '\r')
-#   bootAnim=$(adb shell getprop init.svc.bootanim 2>/dev/null | tr -d '\r')
-#   settingsCheck=$(adb shell service call settings 2>&1)
-
-#   echo "deviceState: $deviceState"
-#   echo "bootComplete: $bootComplete"
-#   echo "bootAnim: $bootAnim"
-#   echo "settingsCheck: $settingsCheck"
-
-    deviceState=$("$androidHome/platform-tools/adb" get-state 2>/dev/null)
-    bootComplete=$("$androidHome/platform-tools/adb" shell getprop sys.boot_completed 2>/dev/null | tr -d '\r')
-    bootAnim=$("$androidHome/platform-tools/adb" shell getprop init.svc.bootanim 2>/dev/null | tr -d '\r')
-    settingsCheck=$("$androidHome/platform-tools/adb" shell service call settings 2>&1)
-
-    echo "deviceState: $deviceState"
-    echo "bootComplete: $bootComplete"
-    echo "bootAnim: $bootAnim"
-    echo "settingsCheck: $settingsCheck"
-
-
-  if [[ "$deviceState" == "device" && "$bootComplete" == "1" && "$bootAnim" == "stopped" && "$settingsCheck" != "Can't find service" ]]; then
+  deviceState=$($ADB_PATH get-state 2>/dev/null)
+  bootComplete=$($ADB_PATH shell getprop sys.boot_completed 2>/dev/null | tr -d '\r')
+  echo "deviceState: $deviceState"
+  echo "bootComplete: $bootComplete"
+  if [[ "$deviceState" == "device" && "$bootComplete" == "1" ]]; then
     break
   fi
-
   sleep 2
 done
 
-echo "Emulator is fully booted and ready"
-echo "Running tests with WebdriverIO..."
+echo "✅ Emulator is fully booted and ready"
+echo "🧪 Running tests with WebdriverIO..."
 npx wdio run wdio.conf.ts
